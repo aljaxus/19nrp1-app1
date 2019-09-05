a9a71f68a2587aa372804b619bc7741f9dea7677
aljaxus <me@aljaxus.eu>
2019-09-05 18:24:37 +0200

    Improved the "Current games" list

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,22 +1,33 @@
 <template>
   <v-card>
     <v-card-text>
-      <v-list rounded>
-        <v-subheader>Current games</v-subheader>
-        <v-list-item-group color="primary">
-          <v-list-item
-            v-for="(item, i) in allgames"
-            :key="i"
+      <v-layout row wrap>
+        <v-flex xs12 md4 lg3 xl2>
+          <v-list
+            rounded
+            dense
           >
-            <v-list-item-icon>
-              <v-icon v-text="item.a" />
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title v-text="item.id" />
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
+            <v-subheader>Current games</v-subheader>
+            <v-list-item-group>
+              <v-list-item
+                v-for="(item, i) in games"
+                :color="item.color"
+                :key="i"
+                :to="{ name: 'game', params: { gameid: item.id } }"
+              >
+                <v-list-item-icon>
+                  <v-icon v-text="item.icon" />
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    {{ item.id }} - {{ item.msg }}
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-flex>
+      </v-layout>
     </v-card-text>
   </v-card>
 </template>
@@ -31,12 +42,14 @@ export default {
       return this.allgames.map(el => {
         let icon = ''
         let color = ''
-        if (el.state === 'waiting') { icon = 'mdi-clock-outline'; color = 'info' }
-        if (el.state === 'starting') { icon = 'mdi-alert'; color = 'warning' }
-        if (el.state === 'ingame') { icon = 'mdi-account-group'; color = 'warning' }
-        if (el.state === 'ending') { icon = 'mdi-stop-circle-outline'; color = 'error' }
+        let msg = ''
+        if (el.state === 'waiting') { icon = 'mdi-clock-outline'; color = 'info--text'; msg = 'In waiting lobby' }
+        if (el.state === 'starting') { icon = 'mdi-alert'; color = 'warning--text'; msg = 'Game is starting' }
+        if (el.state === 'ingame') { icon = 'mdi-account-group'; color = 'warning--text'; msg = 'Game is running' }
+        if (el.state === 'ending') { icon = 'mdi-stop-circle-outline'; color = 'error--text'; msg = 'Game is ending' }
         return {
           ...el,
+          msg,
           icon,
           color,
         }
